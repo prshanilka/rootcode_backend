5cc79e510e6a596a519dd53d7e29629f834e0337
prshanilka <ramesh.shanilka@gmail.com>
2021-03-04 22:00:07 +0530

    re

--- comment/comment.service.js
+++ comment/comment.service.js
@@ -3,7 +3,7 @@ module.exports = {
   createComment: (data,callBack) => {
     pool.query(
       `INSERT INTO comment(c_text,postID) VALUES (?,?)`,
-      [data.comment, data.postID],
+      [data.c_text, data.postID],
       (error, results, fields) => {
         if (error) {
           return callBack(error);
@@ -15,14 +15,14 @@ module.exports = {
   getComment: (post_id, callBack) => {
     console.log(post_id);
     pool.query(
-      `SELECT * FROM post where postID = ?`,
+      `SELECT * FROM comment where postID = ?`,
       [post_id],
       (error, results, fields) => {
         if (error) {
           return callBack(error);
         }
         //console.log(results);
-        return callBack(null, results[0]);
+        return callBack(null, results);
       }
     );
   },
